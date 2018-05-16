0dc5c6ebe6cb464b86b232ececa79f143a109fd9
Ding Yubo <599079736@qq.com>
2018-05-16 11:29:48 +0800

    hystrix feign，ribbon dashboard

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # springcloud-core
-Creating a new branch is quick & simple.
+feign使用hystrix-dashboard需要@EnableCircuitBreaker注解
+ribbon则不用
+
+feign自带断路器hystrix，但是在低版本中需要在application.properties中添加feign.hystrix.enabled=true
+开启
+
+
--- springcloud-feign/src/main/java/com/springcloud/feign/SpringcloudFeignApplication.java
+++ springcloud-feign/src/main/java/com/springcloud/feign/SpringcloudFeignApplication.java
@@ -2,11 +2,15 @@ package com.springcloud.feign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableHystrixDashboard
+@EnableCircuitBreaker //feign中使用hystrix-dashboard需要这个注解
 @SpringBootApplication
 public class SpringcloudFeignApplication {
 
